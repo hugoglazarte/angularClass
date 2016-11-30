45fba5dc5c4c19c235cb7fb8a6da4286d7d99d77
Hugo Lazarte <hugolazarte@MacBook-Pro-de-Hugo.local>
2016-11-30 20:50:44 -0300

    ng-repeat creado

--- js/app.js
+++ js/app.js
@@ -1,5 +1,17 @@
 // console.log(angular) -> con esto vemos si esta angular
 // funcion autoinvocada : IIFE
+/*
+
+{{}} espera string
+
+	ng-if
+
+	ng-require
+
+	ng-show = funciona como if pero siempre esta el elemento en el DOM escondido
+
+*/
+
 (function(){
 	'use strict';
 
@@ -28,11 +40,17 @@
 
 	.controller('ProductController', function($scope) { // en el callback vamos a definir todo lo que pase en el controlador, con scope le damos el ambiente donde va a trabajar
 
-		$scope.image = "http://placehold.it/750x500";
-		$scope.productTitle = "Producto A";
-		$scope.fecha = "22/11/2016";
-		$scope.precio = "$ 100.-"
-		$scope.descripcion = "Producto bla bla bla"
+		$scope.product = {
+
+		image : "http://placehold.it/750x500",
+		productTitle : "Producto A",
+		fecha : "22/11/2016",
+		precio : "$ 100.-",
+		descripcion : "Producto bla bla bla",
+		featured : true,
+		tags: ['computadora','diseno','apple']
+
+		}
 
 	})
 
